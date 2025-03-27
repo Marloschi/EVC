63b74d55cbafa6ae6ef67584b8fcd35d24879fc1
Marloschi <marlo.mitterlehner@gmx.at>
2025-03-27 17:25:12 +0100

    images done

--- Beispiel 2/bsp2_basics/images.py
+++ Beispiel 2/bsp2_basics/images.py
@@ -35,7 +35,8 @@ def switch_channels(img: np.ndarray) -> np.ndarray:
     ### STUDENT CODE
     # TODO: Implement this function.
     out = img.copy()
-    out[:, :, 0], out[:, :, 1] = img[:, :, 1], img[:, :, 0]
+    out[:,:,[0,1]] = out[:,:,[1,0]]
+
     # NOTE: The following lines can be removed. They prevent the framework
     #       from crashing.
 
@@ -50,7 +51,8 @@ def image_mark_green(img: np.ndarray) -> np.ndarray:
     """
     ### STUDENT CODE
     # TODO: Implement this function.
-    mask = (img[:, :, 1] >= 0.7)
+    green_channel = img[:,:,1]
+    mask = (green_channel >= 0.7)
 
     # NOTE: The following lines can be removed. They prevent the framework
     #       from crashing.
@@ -67,7 +69,7 @@ def image_masked(img: np.ndarray, mask: np.ndarray) -> np.ndarray:
     ### STUDENT CODE
     # TODO: Implement this function.
     out = img.copy()
-    out[mask, :] = 0
+    out[mask == 1] = 0
     # NOTE: The following lines can be removed. They prevent the framework
     #       from crashing.
 
@@ -99,12 +101,11 @@ def cut_and_reshape(img_gray: np.ndarray) -> np.ndarray:
     """
     ### STUDENT CODE
     # TODO: Implement this function.
-    sideLength = img_gray.shape[0]
+    height,width = img_gray.shape
 
-    mid = sideLength // 2
-    left = img_gray[:, :mid]
-    right = img_gray[:, mid:]
-    out = np.vstack((left, right))
+    left = img_gray[:, :height//2]
+    right = img_gray[:, width//2:]
+    out = np.vstack((right, left))
 
     # NOTE: The following lines can be removed. They prevent the framework
     #       from crashing.
@@ -122,11 +123,39 @@ def filter_image(img: np.ndarray) -> np.ndarray:
 
     ### STUDENT CODE
     # TODO: Implement this function.
+    height, width, channels = img.shape
+    out = np.zeros_like(img)
 
+    # Kernel size and radius
+    k_size = 5
+    k_radius = k_size // 2
+
+    # Iterate over each pixel in the image
+    for y in range(height):
+        for x in range(width):
+            # Initialize sum for each channel
+            pixel_sum = [0.0, 0.0, 0.0]
+
+            # Iterate over kernel
+            for ky in range(-k_radius, k_radius + 1):
+                for kx in range(-k_radius, k_radius + 1):
+                    # Calculate image coordinates
+                    img_y = y + ky
+                    img_x = x + kx
+
+                    # Check if within image bounds
+                    if (0 <= img_y < height) and (0 <= img_x < width):
+                        # Multiply and accumulate for each channel
+                        for c in range(channels):
+                            pixel_sum[c] += img[img_y, img_x, c] * gaussian[ky + k_radius, kx + k_radius]
+                    # Else: treat as [0,0,0] (no addition needed)
+
+            # Assign the filtered value to output
+            for c in range(channels):
+                out[y, x, c] = pixel_sum[c]
     # NOTE: The following lines can be removed. They prevent the framework
     #       from crashing.
 
-    out = np.zeros(img.shape)
 
     ### END STUDENT CODE
 
@@ -139,11 +168,16 @@ def horizontal_edges(img: np.ndarray) -> np.ndarray:
     """
     ### STUDENT CODE
     # TODO: Implement this function.
+    G_horizontal = np.array([[1, 2, 1],
+                             [0, 0, 0],
+                             [-1, -2, -1]])
 
+    # Apply the filter using convolution
+    out = scipy.ndimage.correlate(img, G_horizontal, mode='constant', cval=0)
     # NOTE: The following lines can be removed. They prevent the framework
     #       from crashing.
 
-    out = np.zeros(img.shape)
+
 
     ### END STUDENT CODE
 
--- Beispiel 2/bsp2_basics/transformations.py
+++ Beispiel 2/bsp2_basics/transformations.py
@@ -3,6 +3,7 @@ from typing import List
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def define_transformations() -> List[np.ndarray]:
     """
         Returns the four transformations t1, .., t4 to transform the quadrat. 
@@ -10,81 +11,102 @@ def define_transformations() -> List[np.ndarray]:
     """
     ### STUDENT CODE
     # TODO: Implement this function.
-
-	# NOTE: The following lines can be removed. They prevent the framework
+    t1 = mtranslate(-3, 0) @ mrotate(55)
+    t2 = mtranslate(-3 * np.cos(np.deg2rad(55)), -3 * np.sin(np.deg2rad(55))) @ mrotate(55)
+    t3 = mtranslate(3, 1) @ mrotate(70) @ mscale(3, 2)
+    t4 = mscale(1, 3) @ mrotate(45)
+    # NOTE: The following lines can be removed. They prevent the framework
     #       from crashing.
 
-    t1, t2, t3, t4 = np.zeros((3,3)), np.zeros((3,3)), np.zeros((3,3)), np.zeros((3,3)),
+
 
     ### END STUDENT CODE
 
     return [t1, t2, t3, t4]
 
-def mscale(sx : float, sy : float) -> np.ndarray:
+
+def mscale(sx: float, sy: float) -> np.ndarray:
     """
         Defines a scale matrix. The scales are determined by sx in x and sy in y dimension.
     """
     ### STUDENT CODE
     # TODO: Implement this function.
-
-	# NOTE: The following lines can be removed. They prevent the framework
+    m = np.array([
+        [sx, 0, 0],
+        [0, sy, 0],
+        [0, 0, 1]
+    ])
+    # NOTE: The following lines can be removed. They prevent the framework
     #       from crashing.
-
-    m = np.zeros((3,3))
 
     ### END STUDENT CODE
 
     return m
 
-def mrotate(angle : float) -> np.ndarray:
+
+def mrotate(angle: float) -> np.ndarray:
     """
         Defines a rotation matrix (z-axis) determined by the angle in degree (!).
     """
     ### STUDENT CODE
     # TODO: Implement this function.
-
-	# NOTE: The following lines can be removed. They prevent the framework
+    theta = np.radians(angle)
+    cos = np.cos(theta)
+    sin = np.sin(theta)
+    m = np.array([
+        [cos, -sin, 0],
+        [sin, cos, 0],
+        [0, 0, 1]
+    ])
+    # NOTE: The following lines can be removed. They prevent the framework
     #       from crashing.
 
-    m = np.zeros((3,3))
+
 
     ### END STUDENT CODE
 
     return m
-    
-def mtranslate(tx : float, ty : float) -> np.ndarray:
+
+
+def mtranslate(tx: float, ty: float) -> np.ndarray:
     """
         Defines a translation matrix. tx in x, ty in y direction.
     """
     ### STUDENT CODE
     # TODO: Implement this function.
-
-	# NOTE: The following lines can be removed. They prevent the framework
+    m = np.array([
+        [1, 0, tx],
+        [0, 1, ty],
+        [0, 0, 1]
+    ])
+    # NOTE: The following lines can be removed. They prevent the framework
     #       from crashing.
 
-    m = np.zeros((3,3))
+
 
     ### END STUDENT CODE
 
     return m
 
-def transform_vertices(v : np.ndarray, m : np.ndarray) -> np.ndarray:
+
+def transform_vertices(v: np.ndarray, m: np.ndarray) -> np.ndarray:
     """
         transform the (3xN) vertices given by v with the (3x3) transformation matrix determined by m.
     """
     ### STUDENT CODE
     # TODO: Implement this function.
-
-	# NOTE: The following lines can be removed. They prevent the framework
+    out = m @ v
+    # NOTE: The following lines can be removed. They prevent the framework
     #       from crashing.
 
-    out = np.zeros((m.shape[0],v.shape[1]))
+
 
     ### END STUDENT CODE
 
     return out
 
-def display_vertices(v : np.ndarray, title : str) -> None:
+
+def display_vertices(v: np.ndarray, title: str) -> None:
     """
         Plot the vertices in a matplotlib figure.
     """
@@ -95,18 +117,18 @@ def display_vertices(v : np.ndarray, title : str) -> None:
     plt.title(title)
 
     # x and y limits
-    plt.xlim((-6,6))
-    plt.ylim((-6,6))
-    plt.xticks(range(-6,6))
-    plt.yticks(range(-6,6))
+    plt.xlim((-6, 6))
+    plt.ylim((-6, 6))
+    plt.xticks(range(-6, 6))
+    plt.yticks(range(-6, 6))
 
     # plot coordinate axis
     plt.axvline(color='black')
     plt.axhline(color='black')
     plt.grid()
-    
+
     # we just add the last element, so plot can do our job :)
-    v_ = np.concatenate((v, v[:, 0].reshape(3,-1)), axis=1)
+    v_ = np.concatenate((v, v[:, 0].reshape(3, -1)), axis=1)
 
     plt.plot(v_[0, :], v_[1, :], linewidth=3)
     plt.show()
